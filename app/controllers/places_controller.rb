0bb66fd89049f14e3fbe341e0fd804a290f25eac
badmanski <stanislav.gorski@gmail.com>
2013-09-20 21:48:13 +0300

    Places new and index available unauthorized

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < InheritedResources::Base
+  skip_before_filter :authorize, only: [:index, :show]
   
   def permitted_params
     params.permit(place: [:name, :email, :phone, :url, :address], )
